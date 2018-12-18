09d307a20cb0691f6a79c1803cf1ce909b267dcc
Mansur Akramov <makra@Mansurs-MacBook-Pro.local>
2018-12-17 17:57:41 -0800

    initial commit

---
+++ Smack/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Smack
+//
+//  Created by Mansur Akramov on 12/17/18.
+//  Copyright © 2018 Mansur Akramov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
