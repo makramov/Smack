8d1b0e83d1ad9edfc41182e9b180778289ed783b
Mansur Akramov <makra@Mansurs-MacBook-Pro.local>
2018-12-19 20:50:24 -0800

    Created Webservice

--- Smeck/Utilities/Utilities.swift
+++ Smeck/Utilities/Utilities.swift
@@ -8,9 +8,23 @@
 
 import Foundation
 
+typealias CompletionHandler = (_ Success: Bool) -> ()
+
+//Url constans
+
+let BASE_URL = "http://localhost:3005/v1/"
+let URL_REGISTER = "\(BASE_URL)account/register"
+
 //segues identifiers
 let loginVC = "goToLogin"
 let channelsVC = "sw_rear"
 let chatVC = "sw_front"
 let gotoCreateVC = "goToCreateAccount"
 let gotoChannel = "unwindToChannel"
+
+
+//user defaults
+
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
